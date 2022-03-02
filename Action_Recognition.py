98f959f287e8c3826286f261098261193fd37641
jignyik <85657250+jignyik@users.noreply.github.com>
2022-03-02 22:01:09 +0800

    Update Action_Recognition.py

--- Action_Recognition.py
+++ Action_Recognition.py
@@ -55,7 +55,7 @@ class PreProcessing:
     @staticmethod
     def load_detectron_config():
         cfg = get_cfg()
-        cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+        cfg.merge_from_file("faster_rcnn_R_50_FPN_3x.yaml")
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.55  # set threshold for this model
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
         predictor = DefaultPredictor(cfg)
